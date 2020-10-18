3588164cf22f0e17843f15d9153084b4c67d1694
Bravo Yeung <870878+geekplayers@user.noreply.gitee.com>
2020-10-18 18:32:08 +0800

    Update.

--- deploy.bash
+++ deploy.bash
@@ -1,14 +1,6 @@
-rm -rf categories
-rm -rf category
 rm -rf tags
-
 bundle exec jekyll build
-
-cp -r _site/categories .
 cp -r _site/tags .
-cp -r _site/category .
 git add tags
-git add categories
-git add category
-git commit -m 'deploy categories and tags.'
+git commit -m 'Deploy tags.'
 # git reset HEAD~ # In case build error occurs, run this command.
